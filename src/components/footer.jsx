e09b0e5618554bc3e3bcb64bed92aa074c9e0db1
surya0180 <teja8551@gmail.com>
2021-11-03 07:58:43 +0530

    Initial setup complete

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './footer.module.css'
+
+const Footer = () => {
+    return <footer>
+        <div className={"container" + " " + classes.heading}>
+        </div>
+        <div className={"container" + " " + classes.content}>
+        </div>
+        <div className={"container" + " " + classes.trailing}>
+        </div>
+    </footer>
+}
+
+export default Footer;
